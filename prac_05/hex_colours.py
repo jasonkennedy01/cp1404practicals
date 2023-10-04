6af229348ecef1032e352f7f94ce7789dbc08013
jasonkennedy01 <jason.kennedy@my.jcu.edu.au>
2023-10-04 14:24:40 +1000

    Add code to hex_colours.py in prac 5

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+
+COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "Ash Grey": "#b2beb5", "Beige": "#f5f5dc", "Black": "#000000",
+                  "Brown": "#a52a2a", "Bubble Gum": "#ffc1cc", "Cardinal": "#c41e3a", "Celtic Blue": "#246bce",
+                  "Chestnut": "#954535", "Corn": "#fbec5d"}
+
+colour_name = input("Enter a colour: ").title()
+while colour_name != "":
+    try:
+        print(colour_name, "is", COLOUR_TO_CODE[colour_name])
+    except KeyError:
+        print("Invalid colour name")
+    colour_name = input("Enter a colour: ").title()
