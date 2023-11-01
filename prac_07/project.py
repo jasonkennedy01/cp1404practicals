526f86908f1598be972ee14d5fef5e6a572f85d0
jasonkennedy01 <jason.kennedy@my.jcu.edu.au>
2023-11-01 11:50:55 +1000

    Create basic class for Project

--- prac_07/project.py
+++ prac_07/project.py
@@ -4,3 +4,20 @@ Estimate: 5 hours
 Time spent:
 11:45 -
 """
+
+
+class Project:
+    """Project Class."""
+
+    def __init__(self, name, date, priority, cost, completion):
+        """Define the fields for the project object."""
+        self.name = name
+        self.date = date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
+
+    def __repr__(self):
+        """Returns a string representation for a project object."""
+        return (f"{self.name}, start: {self.date}, priority {self.priority}, "
+                f"estimate: ${self.cost}, completion: {self.completion}%")
