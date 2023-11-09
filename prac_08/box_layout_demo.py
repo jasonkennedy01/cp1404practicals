6586d9baa16bd7d3fe213c5cc75e2c43d0b4768e
jasonkennedy01 <jason.kennedy@my.jcu.edu.au>
2023-11-09 12:20:13 +1000

    Add docstrings to classes and methods in prac_08

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -3,16 +3,20 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Box layout model for app."""
     def build(self):
+        """Build the app."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
-        print('test')
+        """Handle a button press to display message from text input to an output label."""
+        # print('test')
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Handle a button press to clear text input and label."""
         self.root.ids.input_name.text = ""
         self.root.ids.output_label.text = ""
 
--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -6,22 +6,22 @@ from kivy.uix.label import Label
 
 
 class DynamicLabelsApp(App):
-    """"""
+    """Dynamic Label App model class"""
 
     def __init__(self):
-        """"""
+        """Create the main app."""
         super().__init__()
         self.names = ["Jason Kennedy", "Reece Kelly", "Keanu Reeves", "Joe Money"]
 
     def build(self):
-        """"""
+        """Build the app GUI."""
         self.title = "Dynamic Labels"
         self.root = Builder.load_file("dynamic_labels.kv")
         self.create_widgets()
         return self.root
 
     def create_widgets(self):
-        """"""
+        """Creates labels from data and add them to the GUI."""
         for name in self.names:
             temp_label = Label(text=name)
             self.root.ids.main.add_widget(temp_label)
