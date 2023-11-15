f5c644411a2a51474f3dc2134eea82e87c7e1a34
jasonkennedy01 <jason.kennedy@my.jcu.edu.au>
2023-11-15 10:19:10 +1000

    Refactor price_per_km to class variable

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -7,20 +7,20 @@ from prac_09.car import Car
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
+    price_per_km = 1.23
 
-    def __init__(self, name, fuel, price_per_km):
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
         """Return a string like a Car but with current fare distance."""
-        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${Taxi.price_per_km:.2f}/km"
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        return Taxi.price_per_km * self.current_fare_distance
 
     def start_fare(self):
         """Begin a new fare."""
